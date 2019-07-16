107ac22b8064a0fcd86bfed94c643a42f89e9259
James Wiryo <jamesgiggs95@gmail.com>
2019-07-16 23:29:09 +0800

    Implemented calculator to use variables instead of hardcoded tickers

--- chalicelib/Scraper/scrapeS&P500.py
+++ chalicelib/Scraper/scrapeS&P500.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 
-def get_sp500_ticker_symbol_list():
-    # Read Data from Wikipedia List of S&P500 Companies
-    data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+class Scraper:
 
-    # Get the list of tickerSymbols
-    ticker_table = data[0]
-    tickers_list = ticker_table['Symbol'].tolist()
+    @staticmethod
+    def get_sp500_ticker_symbol_list():
+        # Read Data from Wikipedia List of S&P500 Companies
+        data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 
-    return tickers_list
+        # Get the list of tickerSymbols
+        ticker_table = data[0]
+        tickers_list = ticker_table['Symbol'].tolist()
+
+        return tickers_list
--- chalicelib/calculator/calculateNCAV.py
+++ chalicelib/calculator/calculateNCAV.py
@@ -3,30 +3,38 @@ import json
 from urllib.request import urlopen
 
 
-def calculate_ncav_per_share():
-    # Call API to get balance sheet
-    balance_sheet_url = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/AAPL?period=quarter?datatype=json"
-    balance_sheet_response = urlopen(balance_sheet_url)
-    decoded_balance_sheet_data = balance_sheet_response.read().decode("utf-8")
+class NcavCalculator:
 
-    # Get relevant balance sheet data
-    balance_sheet_data = json.loads(decoded_balance_sheet_data)['financials'][0]
-    total_current_assets = float(balance_sheet_data['Total current assets'])
-    total_liabilities = float(balance_sheet_data['Total liabilities'])
+    @staticmethod
+    def calculate_ncav(ticker):
+        # Call API to get balance sheet
+        balance_sheet_url = 'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/{}?period=quarter?datatype=json'.format(ticker)
+        balance_sheet_response = urlopen(balance_sheet_url)
+        decoded_balance_sheet_data = balance_sheet_response.read().decode("utf-8")
 
-    # Call API to get income statement
-    income_statement_url = "https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL?period=quarter?datatype=json"
-    income_statement_response = urlopen(income_statement_url)
-    decoded_income_statement_data = income_statement_response.read().decode("utf-8")
+        # Get relevant balance sheet data
+        balance_sheet_data = json.loads(decoded_balance_sheet_data)['financials'][0]
+        total_current_assets = float(balance_sheet_data['Total current assets'])
+        total_liabilities = float(balance_sheet_data['Total liabilities'])
 
-    # Get relevant income statement data
-    income_statement_data = json.loads(decoded_income_statement_data)['financials'][0]
-    total_shares_outstanding = float(income_statement_data['Weighted Average Shs Out'])
+        ncav_value = total_current_assets - total_liabilities
+        return ncav_value
 
-    # Calculate NCAV/share
-    ncav_value = total_current_assets - total_liabilities
-    ncav_value_per_share = ncav_value / total_shares_outstanding
-    return ncav_value_per_share
+    @staticmethod
+    def calculate_ncav_per_share(ticker, ncav_value):
+        # Call API to get income statement
+        income_statement_url = "https://financialmodelingprep.com/api/v3/financials/income-statement/{}?period=quarter?datatype=json".format(ticker)
+        income_statement_response = urlopen(income_statement_url)
+        decoded_income_statement_data = income_statement_response.read().decode("utf-8")
+
+        # Get relevant income statement data
+        income_statement_data = json.loads(decoded_income_statement_data)['financials'][0]
+        total_shares_outstanding = float(income_statement_data['Weighted Average Shs Out'])
+
+        # Calculate NCAV/share
+        ncav_value_per_share = ncav_value / total_shares_outstanding
+        return ncav_value_per_share
 
 
-calculate_ncav_per_share()
+calc = NcavCalculator()
+calc.calculate_ncav_per_share('AAPL', calc.calculate_ncav('AAPL'))
